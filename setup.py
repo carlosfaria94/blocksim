64141619cd4f72841f8f1c02ef235a3186126369
CarlosFaria94 <carlosfaria94@gmail.com>
2018-03-13 13:46:19 +0000

    Updates README and adds init files

--- setup.py
+++ setup.py
@@ -1,18 +1,39 @@
 from setuptools import setup, find_packages
-setup(
-    name="BlockSim",
-    version="0.1",
-    packages=find_packages(),
 
-    # metadata for upload to PyPI
-    author="Carlos Faria",
-    author_email="carlosfigueira@tecnico.ulisboa.pt",
-    description="A discrete event Blockchain simulator",
-    license="MIT",
-    keywords="blocksim blockchain simulation discrete-event ethereum",
-    url="https://blockbird.studio",   # project home page, if any
+with open('README.rst') as readme_file:
+    README = readme_file.read()
+
+setup(
+    name='blocksim',
+    version='0.1',
+    packages=find_packages('.'),
+    install_requires=[
+        'simpy',
+        'pysha3>=1.0.1'
+    ],
+    author='Carlos Faria',
+    author_email='carlosfigueira@tecnico.ulisboa.pt',
+    description='A discrete event Blockchain simulator',
+    license='MIT',
+    long_description=README,
+    python_requires='>=3',
+    keywords='blocksim blockchain simulation discrete-event ethereum',
+    url='https://github.com/BlockbirdStudio/blocksim',
     project_urls={
-        "Bug Tracker": "https://github.com/BlockbirdStudio/blocksim/issues",
-        "Source Code": "https://github.com/BlockbirdStudio/blocksim",
-    }
+        'Bug Tracker': 'https://github.com/BlockbirdStudio/blocksim/issues',
+        'Source Code': 'https://github.com/BlockbirdStudio/blocksim/tree/master/blocksim',
+    },
+    classifiers=[
+        'Intended Audience :: Developers',
+        'Intended Audience :: Education',
+        'Intended Audience :: Science/Research',
+        'Natural Language :: English',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Scientific/Engineering',
+    ],
 )
