d6d79001b18679fb79e80bb1931f84b7061a759d
CarlosFaria94 <carlosfaria94@gmail.com>
2018-03-19 10:23:21 +0000

    Add storage

---
+++ blocksim/models/storage.py
@@ -0,0 +1,24 @@
+class Storage:
+    def __init__(self):
+        self.db = {}
+
+    def get(self, key):
+        return self.db[key]
+
+    def put(self, key, value):
+        self.db[key] = value
+
+    def delete(self, key):
+        del self.db[key]
+
+    def commit(self):
+        pass
+
+    def _has_key(self, key):
+        return key in self.db
+
+    def __contains__(self, key):
+        return self._has_key(key)
+
+    def __eq__(self, other):
+        return isinstance(other, self.__class__) and self.db == other.db
