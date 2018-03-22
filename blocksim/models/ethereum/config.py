3a2f0af4bf968e76d9d4325f6c737d6748e04238
CarlosFaria94 <carlosfaria94@gmail.com>
2018-03-22 17:29:49 +0000

    Add default config for Ethereum

---
+++ blocksim/models/ethereum/config.py
@@ -0,0 +1,11 @@
+from blocksim.utils import encode_hex, keccak_256
+
+default_config = dict(
+    GENESIS_DIFFICULTY=131072,
+    GENESIS_GAS_LIMIT=3141592,
+    GENESIS_PREVHASH=encode_hex(b'\x00' * 32),
+    GENESIS_COINBASE=encode_hex(b'\x00' * 20),
+    GENESIS_TIMESTAMP=0,
+    GENESIS_NUMBER=0,
+    BLANK_ROOT='681afa780d17da29203322b473d3f210a7d621259a4e6ce9e403f5a266ff719a'
+)
