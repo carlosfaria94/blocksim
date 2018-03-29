a1e77133db8df30f2da60f10c353fcb731bbf298
CarlosFaria94 <carlosfaria94@gmail.com>
2018-03-29 16:48:36 +0100

    Add transactions message

--- blocksim/models/ethereum/messages.py
+++ blocksim/models/ethereum/messages.py
@@ -30,7 +30,17 @@ class Messages:
             'size': 10 # TODO: Measure the size message
         }
 
-    def get_block_headers(self, block_number: int, max_headers: int, skip: int, reverse: int):
+    def transactions(self, transactions: list):
+        """ Specify (a) transaction(s) that the peer should make sure is included on its
+        transaction queue. Nodes must not resend the same transaction to a peer in the same session.
+        This packet must contain at least one (new) transaction.
+        """
+        return {
+            'id': 2,
+            'transactions': transactions
+        }
+
+    def get_block_headers(self, block_number: int, max_headers: int, reverse: int):
         """ Require peer to return a `block_headers` message.
         Reply must contain a number of block headers, of rising number when `reverse` is `0`,
         falling when `1`, `skip` blocks apart, beginning at `block_number`.
@@ -40,7 +50,6 @@ class Messages:
             'id': 3,
             'block_number': block_number,
             'max_headers': max_headers,
-            'skip': skip,
             'reverse': reverse,
             'size': 10 # TODO: Measure the size message
         }
@@ -50,9 +59,18 @@ class Messages:
         This may contain no block headers if no block headers were able to be returned
         for the `get_block_headers` message.
         """
+        chain = self.origin_node.chain
+        block_headers = []
+        block_hash = chain.get_blockhash_by_number(request.block_number)
+        block_hashes = chain.get_blockhashes_from_hash(block_hash, request.max_headers)
+        for block_hash in block_hashes:
+            block_header = chain.get_block(block_hash).header
+            block_headers.append(block_header)
+        if request.reverse == 0:
+            block_headers.reverse()
         return {
             'id': 4,
-            'block_headers': [],
+            'block_headers': block_headers,
             'size': 10 # TODO: Measure the size message
         }
 
@@ -70,8 +88,13 @@ class Messages:
         """ Reply to `get_block_bodies`. The items in the list are some of the blocks, minus the header.
         This may contain no items if no blocks were able to be returned for the `get_block_bodies` message.
         """
+        chain = self.origin_node.chain
+        block_bodies = []
+        for block_hash in request.hashes:
+            block = chain.get_block(block_hash)
+            block_bodies.append(block)
         return {
             'id': 6,
-            'block_bodies': [],
+            'block_bodies': block_bodies,
             'size': 10 # TODO: Measure the size message
         }
